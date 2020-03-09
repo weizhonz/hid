9c0bdca1563a63175341f407f4ee23c79607f42a
周箫 <apple@apples-MacBook-Pro.local>
2020-03-10 07:33:28 +0800

    change score to assign and remove abs(0

--- draw_figure.py
+++ draw_figure.py
@@ -2,14 +2,16 @@ import matplotlib.pyplot as plt
 plt.figure(figsize=(16, 4))
 plt.subplot(141)
 ax = plt.gca()
-x = [30, 50, 70]
-y1 = [74.66, 75.61,71.54]
-y2 = [76.19, 78.17, 74.59]
+x = [10, 30, 50, 70]
+y1 = [66.19, 74.93, 75.61,71.54]
+y2 = [62.13, 76.19, 78.17, 74.59]
 plt.xlim(0, 100)
 plt.ylim(65, 90)
 ax.invert_xaxis()
-plt.plot(x,y1,'r',label='$Weigths \sim N_k$')
-plt.plot(x,y2,'g',label='$Weigths \sim U_k$')
+
+plt.plot([10, 30], [76.89, 79.97], '.r')
+plt.plot(x, y1, 'r', label='$Weigths \sim N_k$')
+plt.plot(x, y2, 'g', label='$Weigths \sim U_k$')
 
 plt.hlines(79.16, 0, 100, colors = "b", label='Learned Dense Weights (Adam)', linestyles = "dashed")
 plt.hlines(78.13, 0, 100, colors = "black", label='Learned Dense Weights (SGD)', linestyles = "dashed")
@@ -19,13 +21,16 @@ plt.title("Conv2")
 
 plt.subplot(142)
 ax = plt.gca()
-y1 = [83.59, 83.6, 78.98]
-y2 = [84.66, 86.07, 83.2]
+y1 = [73.78, 83.3, 83.6, 78.98]
+y2 = [72.45, 84.44, 86.07, 83.2]
 plt.xlim(0, 100)
 plt.ylim(65, 90)
 ax.invert_xaxis()
-plt.plot(x,y1,'r',label='$Weigths \sim N_k$')
-plt.plot(x,y2,'g',label='$Weigths \sim U_k$')
+
+plt.plot([10, 30], [82.28, 85.31], '.r')
+plt.plot([10], [82.18], '.g')
+plt.plot(x, y1, 'r', label='$Weigths \sim N_k$')
+plt.plot(x, y2, 'g', label='$Weigths \sim U_k$')
 
 plt.hlines(86.66, 0, 100, colors = "b", label='Learned Dense Weights (Adam)', linestyles = "dashed")
 plt.hlines(86.21, 0, 100, colors = "black", label='Learned Dense Weights (SGD)', linestyles = "dashed")
@@ -34,11 +39,14 @@ plt.title("Conv4")
 
 plt.subplot(143)
 ax = plt.gca()
-y1 = [85.81, 86.78, 83.41]
-y2 = [86.51, 88.17, 86.57]
+y1 = [78.27, 86.05, 86.78, 83.41]
+y2 = [73.99, 86.51, 88.17, 86.57]
 plt.xlim(0, 100)
 plt.ylim(65, 90)
 ax.invert_xaxis()
+
+plt.plot([10, 30], [83, 87.18], '.r')
+plt.plot([10], [84.35], '.g')
 plt.plot(x,y1,'r',label='$Weigths \sim N_k$')
 plt.plot(x,y2,'g',label='$Weigths \sim U_k$')
 
@@ -49,13 +57,15 @@ plt.title("Conv6")
 
 plt.subplot(144)
 ax = plt.gca()
-y1 = [87.09, 87.96, 84.63]
-y2 = [87.87, 89.2, 87.57]
+y1 = [67.01, 87.11, 87.96, 84.63]
+y2 = [0, 87.87, 89.2, 87.57]
 plt.xlim(0, 100)
 plt.ylim(65, 90)
 ax.invert_xaxis()
-plt.plot(x,y1,'r',label='$Weigths \sim N_k$')
-plt.plot(x,y2,'g',label='$Weigths \sim U_k$')
+
+plt.plot([10, 30], [84.67, 88], '.r')
+plt.plot(x, y1, 'r', label='$Weigths \sim N_k$')
+plt.plot([30, 50, 70], [87.87, 89.2, 87.57], 'g', label='$Weigths \sim U_k$')
 
 plt.hlines(88.81, 0, 100, colors = "b", label='Learned Dense Weights (Adam)', linestyles = "dashed")
 plt.hlines(89.05, 0, 100, colors = "black", label='Learned Dense Weights (SGD)', linestyles = "dashed")
