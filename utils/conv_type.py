ed295beee68dd5b222d8f42d5051f8cb136d157d
x-zho14ls <18813119938@163.com>
2020-03-18 09:33:31 +0800

    Update conv_type.py

--- utils/conv_type.py
+++ utils/conv_type.py
@@ -36,7 +36,8 @@ class GetSubnet(autograd.Function):
 class SubnetConv(nn.Conv2d):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.weight = self.weight*0.01
+
+        self.weight.data = torch.mul(self.weight.data, 0.01)
         self.mask = nn.Parameter(torch.Tensor(self.weight.size()))
         torch.nn.init.zeros_(self.mask)
 
