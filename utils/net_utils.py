ef013091aa2e657de18f2d5624c4f051d77a59a7
x-zho14ls <18813119938@163.com>
2020-03-19 16:46:39 +0800

    Update net_utils.py

--- utils/net_utils.py
+++ utils/net_utils.py
@@ -123,8 +123,8 @@ def updateScore(model, args):
                 index_nonzero = torch.nonzero(mask1)
                 index_zero = torch.nonzero(mask2)
                 for i in range(K):
+                    print(i, topk_max[i] - topk_min[i])
                     if topk_max[i]-topk_min[i] > 0.1:
-                        print(i, topk_max[i]-topk_min[i])
                         mask_flatten[index_nonzero[idx1[i]]] = False
                         mask_flatten[index_zero[idx2[i]]] = True
 
