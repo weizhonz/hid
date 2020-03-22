824a6dd84d9527f4babcbd528cf23d9ff8c752ba
x-zho14ls <18813119938@163.com>
2020-03-22 10:37:44 +0800

    Update frankle.py

--- models/frankle.py
+++ models/frankle.py
@@ -19,14 +19,11 @@ class Conv2(nn.Module):
         )
 
         self.linear = nn.Sequential(
-            # builder.conv1x1(64 * 16 * 16, 256),
-            builder.conv1x1(64 * 16 * 16, 64),
+            builder.conv1x1(64 * 16 * 16, 256),
             nn.ReLU(),
-            # builder.conv1x1(256, 256),
-            builder.conv1x1(64, 64),
+            builder.conv1x1(256, 256),
             nn.ReLU(),
-            # builder.conv1x1(256, 10),
-            builder.conv1x1(64, 10),
+            builder.conv1x1(256, 10),
         )
 
     def forward(self, x):
