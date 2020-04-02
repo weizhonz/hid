5d2e8b8ee864a0f6133331b9d03a8062370d601e
x-zho14ls <18813119938@163.com>
2020-04-02 18:56:12 +0800

    r

--- trainers/default.py
+++ trainers/default.py
@@ -70,7 +70,7 @@ def train(train_loader, model, criterion, optimizer, epoch, args, writer):
             l.backward()
         else:
             updateScoreDiff(model, l)
-        printModelScore(model, args)
+        # printModelScore(model, args)
         optimizer.step()
 
         # measure elapsed time
